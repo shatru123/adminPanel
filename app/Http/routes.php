cda5ea5485e214197f6fa5038d5ceb0458ca0bcf
Edwin <edwin@codingfaculty>
2016-04-22 19:30:10 -0400

    admin master page setup

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,6 +19,17 @@ Route::auth();
 
 Route::get('/home', 'HomeController@index');
 
-Route::auth();
 
-Route::get('/home', 'HomeController@index');
+
+Route::get('/admin', function(){
+
+
+    return view('admin.index');
+
+
+});
+
+
+
+Route::resource('admin/users', 'AdminUsersController');
+
---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.admin')
+
+
+
+
+@section('content')
+
+
+    <h1>Admin</h1>
+
+
+
+@stop
